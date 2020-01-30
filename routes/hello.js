99d15e9a24ef1dfc953d7417123c7f0efc4ea691
Kent Thai <ket037@ucsd.edu>
2020-01-30 12:27:51 -0800

    node.js lab

--- routes/hello.js
+++ routes/hello.js
@@ -4,7 +4,14 @@
  */
 
 exports.view = function(req, res){
+  var nameToShow = req.param.userName;
+  console.log("name is " + nameToShow)
+  res.render("hello", {
+    "name": nameToShow
+  });
+
   res.render('index', {
   	'name': '',
   });
+
 };
